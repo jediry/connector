27e4a2d68032c6744e04ffc61889a378b70f5d3c
Ryan Saunders <saunders@aggienetwork.com>
2012-09-17 15:51:45 -0700

    Disable state_abbreviations validation, because it doesn't work in "production" for some reason.

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,7 @@ class Address < ActiveRecord::Base
   belongs_to :person
 
   validates :street, :city, :state, :zip, :presence => true
-  validates :state, :inclusion => { :in => state_abbreviations }
+#  TODO: For some reason, this doesn't work in production mode...figure out why
+#  validates :state, :inclusion => { :in => state_abbreviations }
   validates :zip, :format => { :with => /^[0-9]{5}(-[0-9]{4})?$/, :message => "%{value} is not a valid zip code" }
 end
