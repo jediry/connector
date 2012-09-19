d50498b513f8dd629bdd33b98b7ddfc4d6932298
Ryan Saunders <saunders@aggienetwork.com>
2012-09-18 20:57:36 -0700

    Fixed problem with validating :state field in Address model.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,10 @@
+include ApplicationHelper
+
 class Address < ActiveRecord::Base
   attr_accessible :city, :detail, :state, :street, :zip
   belongs_to :person
 
   validates :street, :city, :state, :zip, :presence => true
-#  TODO: For some reason, this doesn't work in production mode...figure out why
-#  validates :state, :inclusion => { :in => state_abbreviations }
+  validates :state, :inclusion => { :in => state_abbreviations }
   validates :zip, :format => { :with => /^[0-9]{5}(-[0-9]{4})?$/, :message => "%{value} is not a valid zip code" }
 end
