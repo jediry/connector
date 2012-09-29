b8ed14ff30e39c8abc35569602fd3c3c826fb817
Ryan Saunders <saunders@aggienetwork.com>
2012-09-29 15:59:27 -0700

    Don't allow inactive users to log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_username(params[:session][:username].downcase)
     if user and user.authenticate(params[:session][:password])
-      # Successfully authenticated. You may pass.
-      log_in user
-      redirect_to home_path
+      # Successfully authenticated
+      if user.active
+        # You may pass
+        log_in user
+        redirect_to home_path
+      else
+        flash.now[:error] = 'Your user account is inactive, so you may not log in.'
+        render 'new'
+      end
     else
       # Uh oh! Wrong u/p, dummy!
       flash.now[:error] = 'Invalid username/password'
