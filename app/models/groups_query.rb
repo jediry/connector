a0d2c14703de0cfc81b4882d5608a3c949f046d4
Ryan Saunders <saunders@aggienetwork.com>
2012-10-09 03:48:03 -0700

    Added search/sort/filter capability for users, groups, people, and tasks.

---
+++ app/models/groups_query.rb
@@ -0,0 +1,41 @@
+class GroupsQuery
+  include ActiveAttr::Model
+
+  attribute :order_by,      :type => String, :default => :name
+  attribute :reverse_order, :type => Boolean, :default => false
+  attribute :active,    :type => String, :default => 'yes'
+  attribute :name_like, :type => String
+  attr_accessible :order_by, :reverse_order, :active, :name_like
+
+  # TODO: Figure out how to merge this with 'get_sort' below
+  def self.sortable_fields
+    [ 'name', 'creation' ]
+  end
+
+  def each(group_type, &block)
+    q = Group.where(:group_type_id => group_type.id)
+    q = q.order(get_sort)
+    if reverse_order?
+      q = q.reverse_order
+    end
+    if !active.blank?
+      q = q.where(:active => active == 'yes')
+    end
+    if !name_like.blank?
+      q = q.where('name LIKE ?', "%#{name_like}%")
+    end
+
+    q.each { |group| yield group }
+  end
+
+private
+  def get_sort
+    if !order_by.blank?
+      return 'name' if order_by == 'name'
+      return 'created_at' if order_by == 'creation'
+    end
+
+    # If no order_by was specified, or if it's invalid, sort by name
+    'name'
+  end
+end
---
+++ app/models/tasks_query.rb
@@ -0,0 +1,55 @@
+class TasksQuery
+  include ActiveAttr::Model
+
+  attribute :order_by,      :type => String, :default => 'creation'
+  attribute :reverse_order, :type => Boolean, :default => true
+  attribute :finished,      :type => String, :default => 'no'
+  attribute :overdue_days,  :type => String
+  attribute :user_name_like, :type => String
+  attribute :person_name_like, :type => String
+  attr_accessible :order_by, :reverse_order, :finished, :overdue_days, :user_name_like, :person_name_like
+
+  # TODO: Figure out how to merge this with 'get_sort' below
+  def self.sortable_fields
+    [ 'creation' ]
+#    [ 'user name', 'person name', 'creation' ]
+  end
+
+  def each(task_type, &block)
+    q = Task.where(:task_type_id => task_type.id)
+    q = q.order(get_sort)
+    if reverse_order?
+      q = q.reverse_order
+    end
+    if !finished.blank?
+      q = q.joins('INNER JOIN task_statuses ON task_statuses.id = task_status_id')
+      q = q.where('task_statuses.finish' => finished == 'yes')
+    end
+    if !overdue_days.blank?
+      q = q.where('attempt_next_contact_by IS NOT NULL')
+      q = q.where('attempt_next_contact_by < ?', Time::current.ago(overdue_days.to_i * 60 * 60 * 24))
+    end
+    if !user_name_like.blank?
+      q = q.joins('INNER JOIN people users ON users.id = user_id')
+      q = q.where('users.name LIKE ?', "%#{user_name_like}%")
+    end
+    if !person_name_like.blank?
+      q = q.joins('INNER JOIN people ON people.id = person_id')
+      q = q.where('people.name LIKE ?', "%#{person_name_like}%")
+    end
+
+    q.each { |task| yield task }
+  end
+
+private
+  def get_sort
+    if !order_by.blank?
+#      return 'users.name' if order_by == 'user name'
+#      return 'people.name' if order_by == 'person name'
+      return 'created_at' if order_by == 'creation'
+    end
+
+    # If no order_by was specified, or if it's invalid, sort by creation date
+    'created_at'
+  end
+end
