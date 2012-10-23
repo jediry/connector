21155a21fcaa3a9e73967f9e38aacf416b0b3edb
Ryan Saunders <saunders@aggienetwork.com>
2012-10-23 02:29:46 -0700

    Added a switch to disable email.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  # Set this to true to enable sending email
+  def email_enabled
+    false
+  end
 end
