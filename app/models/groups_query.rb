257e3d2f377d7d8e494783a7a5036145f05f6f0b
Ryan Saunders <saunders@aggienetwork.com>
2012-10-26 01:08:42 -0700

    Added meeting day to group index page and search options.

--- app/models/groups_query.rb
+++ app/models/groups_query.rb
@@ -3,14 +3,15 @@ class GroupsQuery
 
   attribute :order_by,      :type => String, :default => :name
   attribute :reverse_order, :type => Boolean, :default => false
-  attribute :active,    :type => String, :default => 'yes'
-  attribute :region,    :type => String
+  attribute :active,        :type => String, :default => 'yes'
+  attribute :region,        :type => String
+  attribute :meeting_day,   :type => String
   attribute :name_like, :type => String
-  attr_accessible :order_by, :reverse_order, :active, :region, :name_like
+  attr_accessible :order_by, :reverse_order, :active, :region, :meeting_day, :name_like
 
   # TODO: Figure out how to merge this with 'get_sort' below
   def self.sortable_fields
-    [ 'name', 'creation', 'region' ]
+    [ 'name', 'creation', 'region', 'meeting day' ]
   end
 
   def each(group_type, &block)
@@ -24,6 +25,9 @@ class GroupsQuery
     end
     if !region.blank?
       q = q.where(:region_id => region.to_i)
+    end
+    if !meeting_day.blank?
+      q = q.where(:meeting_day => meeting_day.to_i)
     end
     if !name_like.blank?
       q = q.where('name LIKE ?', "%#{name_like}%")
@@ -39,6 +43,7 @@ private
       return 'created_at' if order_by == 'creation'
       # TODO: make this sort by region name, not ID
       return 'region_id' if order_by == 'region'
+      return 'meeting_day' if order_by == 'meeting day'
     end
 
     # If no order_by was specified, or if it's invalid, sort by name
